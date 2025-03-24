c125063b7223add7267e8ef6d8b4a784c38201d3
Chay Fong Hong <chon0012@student.monash.edu>
2025-03-24 13:46:00 +1100

    Fixing the footer bug - After an admin login then accessing the public page, the footer is gone from the public page

--- templates/layout/login.php
+++ templates/layout/login.php
@@ -21,7 +21,8 @@ $appLocale = Configure::read('App.defaultLocale');
     </title>
     <?= $this->Html->meta('icon') ?>
 
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
 
     <?= $this->Html->css('/landing-detail/css/styles.css') ?>
     <?= $this->Html->css(['fonts', 'cake', 'custom']) ?>
@@ -38,9 +39,9 @@ $appLocale = Configure::read('App.defaultLocale');
         <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
-                <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
+                <li class="nav-item"><a class="nav-link" href="/#about">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="/#services">Services</a></li>
+                <li class="nav-item"><a class="nav-link" href="/#portfolio">Portfolio</a></li>
                 <li class="nav-item"><a class="nav-link" href="<?= $this->Url->build(['controller' => 'Contacts', 'action' => 'add']) ?>">Contact Us</a></li>
                 <li class="nav-item"><a class="nav-link" href="<?= $this->Url->build(['controller' => 'Auth', 'action' => 'login']) ?>">Login</a></li>
             </ul>
@@ -59,7 +60,7 @@ $appLocale = Configure::read('App.defaultLocale');
             <!-- Newsletter Signup -->
             <div class="col-md-3">
                 <h5 class="text-light fw-bold">Sign Up for Our Newsletter</h5>
-                <form method="post" action="/contact#ContactFooter" id="ContactFooter">
+                <form method="post" action="form-control" id="ContactFooter">
                     <div class="input-group">
                         <input type="email" class="form-control" name="contact[email]" placeholder="Enter your email" required>
                         <button type="submit" class="btn btn-primary">Subscribe</button>
