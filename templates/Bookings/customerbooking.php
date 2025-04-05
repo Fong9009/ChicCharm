5541e82ad737a34b3c640e0ac1db01256c2919e5
ccoc0004 <ccoc0004@student.monash.edu>
2025-04-06 08:14:49 +1000

    Booking Page Looks Created and links to dashboard

--- templates/Bookings/customerbooking.php
+++ templates/Bookings/customerbooking.php
@@ -11,23 +11,43 @@
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
             <?= $this->Html->link(__('List Bookings'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('Back to Dashboard'), ['controller' => 'Customers', 'action' => 'dashboard'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column column-80">
-        <div class="bookings form content">
+        <div class="enquiry-form">
             <?= $this->Form->create($booking) ?>
             <fieldset>
                 <legend><?= __('Add Booking') ?></legend>
+                <div>
+                    <p>Please provide booking information, you will be able to choose your stylists next.</p>
+                </div>
+                <div class="form-group mb-3">
+                    <?= $this->Form->control('booking_name', [
+                        'label' => __('Booking Name'),
+                        'type' => 'text',
+                        'class' => 'form-control',
+                        'required' => true,
+                        'placeholder' => __('Enter booking Name'),
+                        'error' => ['class' => 'invalid-feedback'],
+                    ]) ?>
+                </div>
+                <div class="form-group mb-3">
+                    <?= $this->Form->control('booking_date',[
+                        'label' => __('Booking Date'),
+                        'type' => 'date',
+                        'class' => 'form-control',
+                        'required' => true,
+                        'placeholder' => __('Enter booking Date'),
+                        'error' => ['class' => 'invalid-feedback'],
+                       ]) ?>
+                </div>
                 <?php
-                    echo $this->Form->control('booking_name');
-                    echo $this->Form->control('booking_date');
-                    echo $this->Form->control('total_cost');
-                    echo $this->Form->control('remaining_cost');
-                    echo $this->Form->control('customer_id', ['options' => $customers, 'empty' => true]);
-                    echo $this->Form->control('stylists._ids', ['options' => $stylists]);
+                    echo $this->Form->control('total_cost', ['type' => 'hidden', 'value' => 0]);
+                    echo $this->Form->control('remaining_cost', ['type' => 'hidden', 'value' => 0]);
                 ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->button(__('Submit'), ['class' => 'btn btn-primary']) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
