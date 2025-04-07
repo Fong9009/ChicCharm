42ebe610da06b721113d134ac14772a68ee8febf
ccoc0004 <ccoc0004@student.monash.edu>
2025-04-07 17:50:06 +1000

    Booking Page has basic Functionality

--- templates/BookingsStylists/customerstylistadd.php
+++ templates/BookingsStylists/customerstylistadd.php
@@ -13,14 +13,16 @@
             <?= $this->Html->link(__('List Bookings Stylists'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
-    <div class="column column-80">
-        <div class="bookingsStylists form content">
+    <div class="column">
+        <div class="enquiry-form">
             <?= $this->Form->create($bookingsStylist) ?>
             <fieldset>
-                <legend><?= __('Add Bookings Stylist') ?></legend>
+                <legend><?= __('Add Stylist To Booking') ?></legend>
                 <div class="filter-section">
                     <?= $this->Form->create(null, ['type' => 'get', 'url' => ['action' => 'filter']]) ?>
 
+                    <?= $this->Form->hidden('filter', ['value' => '1']) ?>
+                    <?= $this->Form->hidden('booking_id', ['value' => $booking_id]) ?>
                     <!-- Service Dropdown -->
                     <?= $this->Form->control('service_id', [
                         'type' => 'select',
@@ -42,41 +44,51 @@
                     ]) ?>
 
                     <!-- Filter Button -->
-                    <?= $this->Form->button(__('Filter Stylists')) ?>
+                    <?= $this->Form->button(__('Filter Stylists'), ['class' => 'btn btn-primary']) ?>
 
                     <?= $this->Form->end() ?>
                 </div>
-                <table>
-                    <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Service</th>
-                        <th>Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach ($filterStylists as $stylist): ?>
+                <div class="contacts index content">
+                    <table>
+                        <thead>
                         <tr>
-                            <td><?= h($stylist->first_name) ?></td>
-                            <td><?= h($stylist->last_name) ?></td>
-                            <td><?= h($stylist->service_name) ?></td>
-                            <td>
-                                <?php if ($stylist->first_name !== null):  ?>
-                                    <?= $this->Form->postLink(
-                                    'Add Contractor',
-                                    ['controller' => 'BookingsStylists', 'action' => 'addContractor', $stylist->id],
-                                    ['confirm' => 'Are you sure you want to add this contractor?', 'class' => 'btn btn-primary']
-                                ) ?>
-                                <?php endif ?>
-                            </td>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Service</th>
+                            <th>Total Price</th>
+                            <th>Action</th>
                         </tr>
-                    <?php endforeach; ?>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($filterStylists as $stylist): ?>
+                            <tr>
+                                <td><?= h($stylist->first_name) ?></td>
+                                <td><?= h($stylist->last_name) ?></td>
+                                <td><?= h($nameOfService) ?></td>
+                                <td><?= h($totalServicePrice) ?></td>
+                                <td class="actions">
+                                    <?php if ($stylist->first_name !== null):  ?>
+                                        <?= $this->Html->link(
+                                            'Add Stylist',
+                                            [
+                                                'controller' => 'BookingsStylists',
+                                                'action' => 'addStylist',
+                                                $stylist->id,
+                                                $booking_id,
+                                                $this->request->getData('start_time'),
+                                                $this->request->getData('end_time'),
+                                                $totalServicePrice,
+                                            ],
+                                            ['class' => 'btn btn-primary', 'confirm' => 'Add this Stylist To Booking?']
+                                        ) ?>
+                                    <?php endif ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
         </div>
     </div>
 </div>
