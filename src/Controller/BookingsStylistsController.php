55cc867b58bb93ea5f50b21eb54a12cd621aa936
ccoc0004 <ccoc0004@student.monash.edu>
2025-04-07 17:50:06 +1000

    Booking Page has basic Functionality

--- src/Controller/BookingsStylistsController.php
+++ src/Controller/BookingsStylistsController.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Cake\I18n\DateTime;
+
 /**
  * BookingsStylists Controller
  *
@@ -59,6 +61,15 @@ class BookingsStylistsController extends AppController
         $this->set(compact('bookingsStylist', 'bookings', 'stylists'));
     }
 
+
+
+
+
+
+
+
+
+    /*
     //This function is to make sure that we can check that a stylist is available
     public function customerstylistadd($booking_id = null)
     {
@@ -123,7 +134,155 @@ class BookingsStylistsController extends AppController
         $this->set(compact('bookingsStylist', 'bookings', 'filterStylists', 'services', 'booking_id'));
     }
 
+    */
 
+    public function customerstylistadd($booking_id = null)
+    {
+
+        //Check if the booking ID is provided
+        if (!$booking_id) {
+            $this->Flash->error(__('Invalid booking missing booking ID'));
+            return $this->redirect(['controller' => 'Bookings', 'action' => 'customerindex']);
+        }
+
+        //This is to obtain the booking ID's date of booking
+        $booking = $this->BookingsStylists->Bookings->get($booking_id);
+        $bookingDateOnly = $booking->booking_date->format('Y-m-d'); // Format it to 'Y-m-d' for comparison
+
+        //Initialisation of the query Variable
+        $query = null;
+
+        if ($this->request->is(['post']) && $this->request->getData('filter') === '1') {
+            $serviceId = $this->request->getData('service_id');
+            $startTime = $this->request->getData('start_time');
+            $endTime = $this->request->getData('end_time');
+
+            //Makes sure that the time is valid
+            if ($startTime && $endTime) {
+                $startTimeValue = strtotime($startTime);
+                $endTimeValue = strtotime($endTime);
+                if ($endTimeValue <= $startTimeValue) {
+                    $this->Flash->error(__('End time must be later than start time.'));
+
+                    return $this->redirect(['action' => 'customerstylistadd', $booking_id]);
+                }
+            } else {
+                $this->Flash->error(__('Start or End times Cannot be blank'));
+            }
+
+
+            //Stylist filter based on selected service
+            //To ensure that we only get stylists that the client want
+            $query = $this->BookingsStylists->Stylists->find()
+                ->select([
+                    'Stylists.id',
+                    'Stylists.first_name',
+                    'Stylists.last_name',
+                    'Services.service_name',
+                ])
+                ->leftJoinWith('Services')
+                ->where(['Services.id' => $serviceId])
+                ->distinct(['Stylists.id']);
+
+            //Subquery to remove stylists who have conflicting bookings in the case of time and date
+            //Ensures that the client can select their time
+            $query->notMatching('BookingsStylists', function ($q) use ($bookingDateOnly, $startTime, $endTime) {
+                return $q->where([
+                    'BookingsStylists.stylist_date' => $bookingDateOnly, // Ensure the booking date matches with booking
+                    'BookingsStylists.start_time <' => $endTime, // Existing booking ends after new start time
+                    'BookingsStylists.end_time >' => $startTime,  // Existing booking starts before new end time
+                    'BookingsStylists.start_time IS NOT NULL',
+                    'BookingsStylists.end_time IS NOT NULL',
+                ]);
+            });
+        }
+        $bookingsStylist = $this->BookingsStylists->newEmptyEntity();
+        //If nothing was obtained such as no available contractor
+        if ($query === null) {
+            $query = null;
+        }
+
+        $startTime = $this->request->getData('start_time');
+        $endTime = $this->request->getData('end_time');
+        $serviceId = $this->request->getData('service_id');
+        $nameOfService = "";
+        $totalServicePrice = null;
+
+        if ($serviceId !== null) {
+            $totalServicePrice = $this->calculateServiceCost($serviceId, $startTime, $endTime);
+            $nameOfService = $this->findServiceName($serviceId);
+        }
+
+        //Paginate the results if query is valid
+        $filterStylists = $this->paginate($query);
+        //Fetch the list of services and bookings
+        $services = $this->BookingsStylists->Stylists->Services->find('list')->toArray();
+        $bookings = $this->BookingsStylists->Bookings->find('list', limit: 200)->all();
+        //Pass the data to the view
+        $this->set(compact(
+            'bookingsStylist',
+            'bookings',
+            'filterStylists',
+            'nameOfService',
+            'totalServicePrice',
+            'services',
+            'booking_id',
+        ));
+    }
+
+    //Used to Calculate the cost of the service by the time it takes
+    private function calculateServiceCost($serviceId, $startTime, $endTime)
+    {
+        //Obtain the cost of the service Per hour
+        $servicesTable = $this->fetchTable('Services');
+        $service = $servicesTable->get($serviceId);
+        $serviceCost = $service->service_cost;
+
+        //Obtain the total cost
+        $startTimeValue = new DateTime($startTime);
+        $endTimeValue = new DateTime($endTime);
+        $difference = $endTimeValue->diff($startTimeValue);
+        $hours = $difference->h;
+        if ($difference->i > 0) {
+            $hours ++;
+        }
+
+        return number_format($hours * $serviceCost,2);
+    }
+
+    //Used to find the service name from given id of stylist
+    private function findServiceName($serviceId)
+    {
+        $servicesTable = $this->fetchTable('Services');
+        $service = $servicesTable->get($serviceId);
+        return $service->service_name;
+    }
+
+    //This is to add a stylist into the bookings Stylists Controller
+    public function addStylist($stylistId, $bookingId, $startTime, $endTime, $totalServicePrice){
+        // This is to add the stylist to
+        $booking = $this->BookingsStylists->Bookings->get($bookingId);
+        $newStylistBooking = $this->BookingsStylists->newEmptyEntity();
+        $newStylistBooking->booking_id = $bookingId;
+        $newStylistBooking->stylist_id = $stylistId;
+        $newStylistBooking->start_time = $startTime;
+        $newStylistBooking->end_time = $endTime;
+        
+        //Ensure still a Float
+        $numberServicePrice = (float)$totalServicePrice;
+        if (is_numeric($numberServicePrice)) {
+            $newStylistBooking->selected_cost = number_format($numberServicePrice, 2);
+        }
+        $newStylistBooking->stylist_date = $booking->booking_date;
+
+        if ($this->BookingsStylists->save($newStylistBooking)) {
+            $this->Flash->success(__('Contractor added successfully.'));
+        } else {
+            $this->Flash->error(__('Failed to add contractor.'));
+        }
+
+        return $this->redirect(['action' => 'customerstylistadd', $bookingId]);
+    }
 
     /**
      * Edit method
