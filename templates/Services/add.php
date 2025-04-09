d9bedd2bad5183ef3c670d7cd76446f575578043
ccoc0004 <ccoc0004@student.monash.edu>
2025-04-09 22:12:55 +1000

    Look Improvements

--- templates/Services/add.php
+++ templates/Services/add.php
@@ -5,25 +5,41 @@
  * @var \Cake\Collection\CollectionInterface|string[] $stylists
  */
 ?>
-<div class="row">
-    <aside class="column">
+<div class="row" style="padding: 100px;">
+    <aside class="column column-80">
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('Back To Dashboard'), ['controller' => 'Admins','action' => 'dashboard'], ['class' => 'side-nav-item']) ?>
             <?= $this->Html->link(__('List Services'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column column-80">
-        <div class="services form content">
+        <div class="enquiry-form">
             <?= $this->Form->create($service) ?>
             <fieldset>
                 <legend><?= __('Add Service') ?></legend>
-                <?php
-                    echo $this->Form->control('service_name');
-                    echo $this->Form->control('service_cost');
-                    echo $this->Form->control('stylists._ids', ['options' => $stylists]);
-                ?>
+                <div class="form-group mb-3">
+                    <?= $this->Form->control('service_name', [
+                            'label' => __('Service Name'),
+                            'type' => 'text',
+                            'class' => 'form-control',
+                            'required' => true,
+                            'placeholder' => __('Enter Service Name'),
+                            'error' => ['Please enter Service Name'],
+                        ]); ?>
+                </div>
+                <div class="form-group mb-3">
+                    <?= $this->Form->control('service_cost', [
+                        'label' => __('Service Cost'),
+                        'type' => 'text',
+                        'class' => 'form-control',
+                        'required' => true,
+                        'placeholder' => __('Enter Service Cost'),
+                        'error' => ['Please enter Service Cost'],
+                    ]); ?>
+                </div>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->button(__('Submit'), ['class' => 'btn btn-primary']) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
