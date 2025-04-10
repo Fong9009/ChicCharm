10ca2f8afedf4258c80526639a641973ad720304
ccoc0004 <ccoc0004@student.monash.edu>
2025-04-10 19:18:10 +1000

    Completed View look for Stylists

--- templates/Bookings/view.php
+++ templates/Bookings/view.php
@@ -4,7 +4,100 @@
  * @var \App\Model\Entity\Booking $booking
  */
 ?>
+<?= $this->Html->css('/views/views.css') ?>
+<?= $this->Html->css('https://fonts.googleapis.com/icon?family=Material+Icons') ?>
 <div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <!-- Edit Stylists -->
+            <div class="row px-2">
+                <div class="col-lg-3 mb-4 side-nav-item">
+                    <?= $this->Html->link(
+                        '<div class="card h-100">
+                            <div class="card-header view-card-header d-flex justify-content-between align-items-center">
+                                <div>
+                                    <h4 class="view-card-h4">Edit Booking</h4>
+                                </div>
+                                <i class="material-icons view-icon">edit</i>
+                            </div>
+                            <div class="card-body view-card-body"></div>
+                            <div class="card-footer view-card-footer">
+                                <span>Edit Bookings</span>
+                            </div>
+                        </div>',
+                        ['controller' => 'Stylists', 'action' => 'edit', $booking->id],
+                        ['escape' => false, 'class' => 'card-link-wrapper d-block text-decoration-none']
+                    ) ?>
+                </div>
+                <!-- Delete Stylist -->
+                <div class="col-lg-3 mb-4 side-nav-item">
+                    <?= $this->Html->link(
+                        '<div class="card h-100">
+                            <div class="card-header delete-card-header d-flex justify-content-between align-items-center">
+                                <div>
+                                    <h4 class="view-card-h4">Delete Stylist</h4>
+                                </div>
+                                <i class="material-icons view-icon">delete</i>
+                            </div>
+                            <div class="card-body delete-card-body"></div>
+                            <div class="card-footer delete-card-footer">
+                                <span>Delete Stylist</span>
+                            </div>
+                        </div>',
+                        ['controller' => 'Stylists', 'action' => 'delete', $booking->id],
+                        ['escape' => false,'class' => 'card-link-wrapper d-block text-decoration-none',
+                            'confirm' => __('Are you sure you want to delete # {0}?', $booking->id)],
+                    ) ?>
+                </div>
+
+                <!-- List Stylist -->
+                <div class="col-lg-3 mb-4 side-nav-item">
+                    <?= $this->Html->link(
+                        '<div class="card h-100">
+                            <div class="card-header list-card-header d-flex justify-content-between align-items-center">
+                                <div>
+                                    <h4 class="view-card-h4">List Stylists</h4>
+                                </div>
+                                <i class="material-icons view-icon">menu</i>
+                            </div>
+                            <div class="card-body list-card-body"></div>
+                            <div class="card-footer list-card-footer">
+                                <span>List Stylists</span>
+                            </div>
+                        </div>',
+                        ['controller' => 'Stylists', 'action' => 'index'],
+                        ['escape' => false, 'class' => 'card-link-wrapper d-block text-decoration-none']
+                    ) ?>
+                </div>
+
+                <!-- New Stylist -->
+                <div class="col-lg-3 mb-4 side-nav-item">
+                    <?= $this->Html->link(
+                        '<div class="card h-100">
+                            <div class="card-header new-card-header d-flex justify-content-between align-items-center">
+                                <div>
+                                    <h4 class="view-card-h4">Create New Stylist</h4>
+                                </div>
+                                <i class="material-icons view-icon">add</i>
+                            </div>
+                            <div class="card-body new-card-body"></div>
+                            <div class="card-footer new-card-footer">
+                                <span>Add Stylist</span>
+                            </div>
+                        </div>',
+                        ['controller' => 'Stylists', 'action' => 'add'],
+                        ['escape' => false, 'class' => 'card-link-wrapper d-block text-decoration-none']
+                    ) ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2" style="padding: 10px 20px;">
+                    <?= $this->Html->link(__('Back to Dashboard'), ['controller' => 'Admins', 'action' => 'dashboard'], ['class' => 'btn btn-primary text-white', 'style' => 'white-space: nowrap;']) ?>
+                </div>
+            </div>
+        </div>
+    </aside>
     <aside class="column">
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
