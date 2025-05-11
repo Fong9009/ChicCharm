1724451a038177aca96d5b59b57dc56e2c7a3df4
ccoc0004 <ccoc0004@student.monash.edu>
2025-05-11 21:15:44 +1000

    FAQ Fix

--- templates/FAQ/questions.php
+++ templates/FAQ/questions.php
@@ -3,64 +3,90 @@
 ?>
 <?= $this->Html->css('faq') ?>
 <?= $this->Html->script('/js/faq.js') ?>
-<!--<!DOCTYPE html>
-<html>
-    <head>
-        <title>FAQ Page</title>
-    </head>-->
+<div class="customer-dashboard" style="background-image: url(<?= $this->Url->image('gradient.jpg')?>);">
+<div class="faq-container">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <div class = "title">
+        <h2 class = "faq-header"><?= $this->ContentBlock->text('faq-title'); ?></h2>
+            <p><?= $this->ContentBlock->text('faq-preamble'); ?></p>
+    </div>
 
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-        <div class = "title">
-            <h2 class = "faq_header">Frequently Asked Questions</h2>
-                <p>This is the place where we answer all your questions! (placeholder)</p>
+    <div class ="faq">
+        <div class ="question">
+            <h3>Question 1: <?= $this->ContentBlock->text('faq-question-title-1'); ?></h3>
+            <svg width = "15" height = "10" viewBox="0 0 42 25">
+                <path d="M3 3L21 21L39 3" stroke="white" stroke-width="1" stroke-linecap="round"/>
+            </svg>
         </div>
-
-        <div class = "faq">
-            <div class =  "question">
-                <h3>Question 1</h3>
-                <svg width = "15" height = "10" viewBox="0 0 42 25">
-                    <path d="M3 3L21 21L39 3" stroke="white" stroke-width="1" stroke-linecap="round"/>
-                </svg>
-            </div>
-            <div class="answer">
-                <p>Answer 1 is this xyz</p>
-            </div>
+        <div class="answer">
+            <p><?= $this->ContentBlock->text('faq-question-desc-1'); ?></p>
         </div>
-
-<div class = "faq">
-    <div class =  "question">
-        <h3>Question 2</h3>
-        <svg width = "15" height = "10" viewBox="0 0 42 25">
-            <path d="M3 3L21 21L39 3" stroke="white" stroke-width="1" stroke-linecap="round"/>
-        </svg>
+        <hr>
     </div>
-    <div class="answer">
-        <p>Answer 1 is this xyz</p>
+
+    <div class ="faq">
+        <div class = "question">
+            <h3>Question 2: <?= $this->ContentBlock->text('faq-question-title-2'); ?></h3>
+            <svg width = "15" height = "10" viewBox="0 0 42 25">
+                <path d="M3 3L21 21L39 3" stroke="white" stroke-width="1" stroke-linecap="round"/>
+            </svg>
+        </div>
+        <div class="answer">
+            <p><?= $this->ContentBlock->text('faq-question-desc-2'); ?></p>
+        </div>
+        <hr>
+    </div>
+
+    <div class ="faq">
+        <div class =  "question">
+            <h3>Question 3: <?= $this->ContentBlock->text('faq-question-title-3'); ?></h3>
+            <svg width = "15" height = "10" viewBox="0 0 42 25">
+                <path d="M3 3L21 21L39 3" stroke="white" stroke-width="1" stroke-linecap="round"/>
+            </svg>
+        </div>
+        <div class="answer">
+            <p><?= $this->ContentBlock->text('faq-question-desc-3'); ?></p>
+        </div>
+        <hr>
+    </div>
+
+    <div class ="faq">
+        <div class ="question">
+            <h3>Question 4: <?= $this->ContentBlock->text('faq-question-title-4'); ?></h3>
+            <svg width = "15" height = "10" viewBox="0 0 42 25">
+                <path d="M3 3L21 21L39 3" stroke="white" stroke-width="1" stroke-linecap="round"/>
+            </svg>
+        </div>
+        <div class="answer">
+            <p><?= $this->ContentBlock->text('faq-question-desc-4'); ?></p>
+        </div>
+        <hr>
+    </div>
+
+    <div class ="faq">
+        <div class ="question">
+            <h3>Question 5: <?= $this->ContentBlock->text('faq-question-title-5'); ?></h3>
+            <svg width = "15" height = "10" viewBox="0 0 42 25">
+                <path d="M3 3L21 21L39 3" stroke="white" stroke-width="1" stroke-linecap="round"/>
+            </svg>
+        </div>
+        <div class="answer">
+            <p><?= $this->ContentBlock->text('faq-question-desc-5'); ?></p>
+        </div>
+        <hr>
+    </div>
+
+    <div class ="faq">
+        <div class ="question">
+            <h3>Question 6: <?= $this->ContentBlock->text('faq-question-title-6'); ?></h3>
+            <svg width = "15" height = "10" viewBox="0 0 42 25">
+                <path d="M3 3L21 21L39 3" stroke="white" stroke-width="1" stroke-linecap="round"/>
+            </svg>
+        </div>
+        <div class="answer">
+            <p><?= $this->ContentBlock->text('faq-question-desc-6'); ?></p>
+        </div>
+        <hr>
     </div>
 </div>
-
-<div class = "faq">
-    <div class =  "question">
-        <h3>Question 3</h3>
-        <svg width = "15" height = "10" viewBox="0 0 42 25">
-            <path d="M3 3L21 21L39 3" stroke="white" stroke-width="1" stroke-linecap="round"/>
-        </svg>
-    </div>
-    <div class="answer">
-        <p>Answer 1 is this xyz</p>
-    </div>
 </div>
-
-<div class = "faq_last">
-    <div class =  "question">
-        <h3>Question 4</h3>
-        <svg width = "15" height = "10" viewBox="0 0 42 25">
-            <path d="M3 3L21 21L39 3" stroke="white" stroke-width="1" stroke-linecap="round"/>
-        </svg>
-    </div>
-    <div class="answer">
-        <p>Answer 1 is this xyz</p>
-    </div>
-</div>
-
-<!--</html>-->
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -357,6 +357,9 @@ $isPublicPage = $this->request->getParam('controller') === 'Contacts' && $this->
                 <ul class="list-unstyled">
                     <li><a href="<?= $this->Url->build('/contacts/enquiry') ?>" class="text-secondary">Contact us</a></li>
                 </ul>
+                <ul class="list-unstyled">
+                    <li><a href="<?= $this->Url->build('/faq/questions') ?>" class="text-secondary">FAQ</a></li>
+                </ul>
             </div>
 
             <!-- Our Company -->
